55a9bb95317733cdf7b2a570ef15001a636a67d9
Vlad Konovalchuk <konovalchuk.vladyslav@steelkiwi.com>
2020-04-19 22:48:40 +0300

    Refactored config of sequelize

--- config/config.js
+++ config/config.js
@@ -3,12 +3,10 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    host: '127.0.0.1',
     dialect: 'postgres',
     use_env_variable: "DATABASE_URL"
   },
   test: {
-    host: '127.0.0.1',
     dialect: 'postgres',
     use_env_variable: "DATABASE_URL"
   },
