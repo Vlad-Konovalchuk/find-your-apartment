a48201224f07c39ebaba7df808e45799c1ae1ebc
Vlad Konovalchuk <konovalchuk.vladyslav@steelkiwi.com>
2020-04-19 17:57:46 +0300

    Fixed config file for sequelize

--- config/config.js
+++ config/config.js
@@ -3,18 +3,14 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
     host: '127.0.0.1',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    use_env_variable: "DATABASE_URL"
   },
   test: {
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
     host: '127.0.0.1',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    use_env_variable: "DATABASE_URL"
   },
   production: {
     username: process.env.DATABASE_USER,
@@ -22,6 +18,6 @@ module.exports = {
     database: process.env.DATABASE,
     host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
-    use_env_variable: process.env.DATABASE_URL
+    use_env_variable: "DATABASE_URL"
   }
 };
